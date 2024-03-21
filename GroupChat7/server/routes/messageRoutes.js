31a9bb71499636e50b476ac0d7f8a703ebb38e01
iqbal1704 <iqbalmipa17@gmail.com>
2024-03-21 14:11:03 +0800

    addGroup fitur

--- GroupChat7/server/routes/messageRoutes.js
+++ GroupChat7/server/routes/messageRoutes.js
@@ -6,5 +6,9 @@ const router = express.Router()
 
 router.get("/:id", protecRoute, getMessages)
 router.post("/send/:id", protecRoute, sendMessage)
+// router.post("/groups/create", protecRoute, createdGroupChat)
+// router.post("/groups/rename", protecRoute, renameGroup)
+// router.delete("/groups/:id", protecRoute, removeFromGroup)
+// router.post("/groups:id", protecRoute, addToGroup)..
 
 export default router
